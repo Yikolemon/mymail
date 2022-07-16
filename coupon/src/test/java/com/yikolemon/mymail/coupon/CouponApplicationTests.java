e446e771535921aaddbeeb597204b1bf8b250a47
Yikolemon <1142496307@qq.com>
2022-07-16 15:53:16 +0800

    项目初始结构

---
+++ coupon/src/test/java/com/yikolemon/mymail/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yikolemon.mymail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ member/src/test/java/com/yikolemon/mymail/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yikolemon.mymail.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
